f45b7b70af68d7cc63a87cf2515d32b46e5b0509
A-Farabi <farabisyl2017@gmail.com>
2025-09-03 02:52:53 +0600

    navbar setuped except avatar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,96 @@
-import React from 'react';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1 className='font-heading color-white'>navbar</h1>
-            <h1 className='font-regular color-one'>navbar</h1>
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <NavLink>Home</NavLink>
+              </li>
+              <li>
+                <NavLink>Trainers</NavLink>
+              </li>
+              <li>
+                <NavLink>All-Classes</NavLink>
+              </li>
+              <li>
+                <NavLink>Dashboard</NavLink>
+              </li>
+              <li>
+                <NavLink>Forums</NavLink>
+              </li>
+              <li>
+                <NavLink>Login</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="btn btn-ghost text-xl w-[42%] lg:w-[20%] bg-black">
+            <img src="/src/assets/Aevium.png" alt="" />
+          </div>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink>Home</NavLink>
+            </li>
+            <li>
+              <NavLink>Trainers</NavLink>
+            </li>
+            <li>
+              <NavLink>All-Classes</NavLink>
+            </li>
+            <li>
+              <NavLink>Dashboard</NavLink>
+            </li>
+            <li>
+              <NavLink>Forums</NavLink>
+            </li>
+            <li>
+              <NavLink>Login</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
